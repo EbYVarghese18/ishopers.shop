3002f47e1409af6b2b52515ab385fa2f993c3fd2
EbYVarghese18 <ebyblr2k18@gmail.com>
2022-11-24 21:20:58 +0530

    first commit

---
+++ user/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')    
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def store(request):
+    return render(request, 'store.html')
