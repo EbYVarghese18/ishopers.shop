2ec21342684eae47eade101e42715190468e8e83
EbYVarghese18 <ebyblr2k18@gmail.com>
2023-01-12 09:15:50 +0530

    app password changed

--- accounts/views.py
+++ accounts/views.py
@@ -43,28 +43,21 @@ def register(request):
             )
             user.phone_number = phone_number
             user.save()
-
             #user activation
-            try:
-                current_site = get_current_site(request)
-                mail_subject = 'Plesae activate your account'
-                message = render_to_string('accountactivationemail.html', {
-                    'user': user,
-                    'domain': current_site,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': default_token_generator.make_token(user),
-                })
-                to_email = email
-                send_email = EmailMessage(mail_subject, message, to=[to_email])
-                send_email.send()
+            current_site = get_current_site(request)
+            mail_subject = 'Plesae activate your account'
+            message = render_to_string('accountactivationemail.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            messages.success(request, 'Thank you for registering with us. We have sent you a verification mail to your email address. Please verify it.')
+        return redirect('signin')
 
-                messages.success(request, 'Thank you for registering with us. We have sent you a verification mail to your email address. Please verify it.')
-
-            #return redirect('?command=verification&email='+email)
-            except:
-                messages.error(request, "We couldn't recognise your mail address. Please enter a valid email")
-
-            return redirect('signin')
     else:
         form = RegistrationForm()
 
